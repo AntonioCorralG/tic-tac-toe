d40d72249a1cbd56578c57e959f872b01e8723b1
Gonzalo <gacorral@pm.me>
2024-02-13 20:51:06 -0800

    updates JS file for assignment tt3

--- tt2Creation.js
+++ tt2Creation.js
@@ -1,5 +1,16 @@
 const NUM_SQUARES = 9;
 for (i = 0; i < NUM_SQUARES; i++) {
-  id = "sq" + i;
+  id = i;
   document.write("<div class='square' id='" + id + "'></div>");
 }
+
+let tttBox = document.querySelectorAll(".square");
+tttBox.forEach(function (clickedBox) {
+  clickedBox.addEventListener("click", function (e) {
+    console.log("something was clicked");
+     squareClicked = document.getElementById(e.target.id)
+     squareClicked.textContent = 'X'
+     alert(`Square number ${e.target.id} was clicked!`)
+    console.log(squareClicked)
+  });
+});
