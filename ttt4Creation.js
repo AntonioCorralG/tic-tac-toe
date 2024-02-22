72c89ff8a033f5d496d1c25dde032f0590331601
Gonzalo <gacorral@pm.me>
2024-02-22 11:42:15 -0800

    add reset button and moves tracking

--- ttt4Creation.js
+++ ttt4Creation.js
@@ -1,31 +1,60 @@
-const NUM_SQUARES = 9;
-for (i = 0; i < NUM_SQUARES; i++) {
-  id = i;
-  document.write("<div class='square' id='" + id + "'></div>");
-}
+window.addEventListener("load", function () {
+  const gridContainer = document.getElementById("grid-container");
+  const NUM_SQUARES = 9;
+  let moves = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+  for (i = 0; i < NUM_SQUARES; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.id = i;
+    gridContainer.appendChild(square);
+  }
 
-let turn = startingPlayer();
+  let turn = startingPlayer();
+  document.getElementById("startGame").addEventListener("click", resetGame);
 
-function startingPlayer() {
-  let randomChoice = Math.random() < 0.5;
-  startingPlayer = randomChoice ? "X" : "O";
-  alert(`The starting player is ${startingPlayer}`);
-  return startingPlayer;
-}
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  function startingPlayer() {
+    let randomChoice = Math.random() < 0.5;
+    startingPlayer = randomChoice ? "X" : "O";
+    alert(`The starting player is ${startingPlayer}`);
+    return startingPlayer;
+  }
 
-let tttBox = document.querySelectorAll(".square");
-tttBox.forEach(function (clickedBox) {
-  clickedBox.addEventListener("click", function (e) {
-    squareClicked = document.getElementById(e.target.id);
+  function resetGame() {
+    moves = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+    document.querySelectorAll(".square").forEach((square) => {
+      square.textContent = "";
+    });
+  }
 
-    if (squareClicked.textContent === "" && turn === "X") {
-      squareClicked.textContent = "X";
-      document.getElementById("nextTurn").innerText = `Next Turn: O`;
-      turn = "O";
-    } else if (squareClicked.textContent === "" && turn === "O") {
-      squareClicked.textContent = "O";
-      document.getElementById("nextTurn").innerText = `Next Turn: X`;
-      turn = "X";
-    }
+  let tttBox = document.querySelectorAll(".square");
+  tttBox.forEach(function (clickedBox) {
+    clickedBox.addEventListener("click", function (e) {
+      squareClicked = document.getElementById(e.target.id);
+
+      if (squareClicked.textContent === "" && turn === "X") {
+        squareClicked.textContent = "X";
+        document.getElementById("nextTurn").innerText = `Next Turn: O`;
+        turn = "O";
+        moves[squareClicked.id] = turn;
+      } else if (squareClicked.textContent === "" && turn === "O") {
+        squareClicked.textContent = "O";
+        document.getElementById("nextTurn").innerText = `Next Turn: X`;
+        turn = "X";
+        moves[squareClicked.id] = turn;
+      }
+
+      console.log(moves);
+    });
   });
+  resetGame();
 });
